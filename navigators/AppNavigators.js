a42d4e3a8e5e34c635cc54e1778cdc90a3b26b52
yeyuying <yeyuying@baobeigezi.com>
2018-07-02 16:01:10 +0800

    使用AppTabNavigator,改变主题

--- navigators/AppNavigators.js
+++ navigators/AppNavigators.js
@@ -1,51 +1,77 @@
-import {StackNavigator, TabNavigator} from "react-navigation"
+import {StackNavigator, TabNavigator, TabBarBottom} from "react-navigation"
 import HomePage from '../pages/homePage';
 import Page1 from '../pages/Page1';
 import Page2 from '../pages/Page2';
 import Page3 from '../pages/Page3';
 import React from 'react';
-import {Button} from 'react-native';
+import {Button, Platform} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+class TabBarComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.theme = {
+            tintColor: props.activeTintColor,
+            updateTime: new Date().getTime()
+        };
+    }
+    render(){
+        const {routes, index} = this.props.navigationState;
+        const {theme} = routes[index].params;
+        if(theme && theme.updateTime>this.theme.updateTime){
+            this.theme = theme;
+        }
+        return <TabBarBottom
+            {...this.props}
+            activeTintColor = {this.theme.tintColor || this.props.activeTintColor}
+        />
+    }
+}
 export const AppTabNavigator = TabNavigator({
     Page1: {
         screen:Page1,
         navigationOptions: {
             tabBarLabel: 'page1',//底部标题
-            tabBarIcon: (({tintColor, focused}) => (//底部图标
+            tabBarIcon: ({tintColor, focused}) => (//底部图标
                 <Ionicons
                     name={focused?'ios-home':'ios-home-outline'}
                     size={26}
                     style={{color:tintColor}}
                     />
-            ))
-        },
+            ),
+        }
     },
-    Page1: {
-        screen: Page1,
+    Page2: {
+        screen: Page2,
         navigationOptions: {
             tabBarLabel: 'Page2',
-            tabBarIcon: (tintColor, focused) => (
+            tabBarIcon: ({tintColor, focused}) => (
                 <Ionicons
                     name={focused ? 'ios-people': 'ios-people-outline' }
                     size ={26}
                     style={{color: tintColor}}
                     />
-            )
+            ),
         }
     },
     Page3: {
-        screen: Page1,
+        screen: Page3,
         navigationOptions: {
             tabBarLabel: 'Page3',
-            tabBarIcon: (tintColor, focused) => (
+            tabBarIcon: ({tintColor, focused}) => (
                 <Ionicons
                     name={focused ? 'ios-chatboxes': 'ios-chatboxes-outline' }
                     size ={26}
                     style={{color: tintColor}}
                     />
-            )
+            ),
         }
+    }
+}, {
+    //tabBarPosition: 'bottom',
+    tabBarComponent: TabBarComponent,
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? '#e91e61' : '#e64303',
+
     }
 })
 export const AppStackNavigator = StackNavigator({
