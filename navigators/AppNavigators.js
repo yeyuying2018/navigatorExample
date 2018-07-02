cd76b350d72eb8e9bc0de016c4bd66081724e005
yeyuying <yeyuying@baobeigezi.com>
2018-07-02 11:18:34 +0800

    动态改变page3头部标题,给其添加右边按钮，并动态改变右边按钮内容

--- navigators/AppNavigators.js
+++ navigators/AppNavigators.js
@@ -1,10 +1,10 @@
-
 import {StackNavigator} from "react-navigation"
 import HomePage from '../pages/homePage';
 import Page1 from '../pages/Page1';
 import Page2 from '../pages/Page2';
 import Page3 from '../pages/Page3';
-
+import React from 'react';
+import {Button} from 'react-native';
 export const AppStackNavigator = StackNavigator({
     HomePage: {
         screen: HomePage
@@ -24,11 +24,33 @@ export const AppStackNavigator = StackNavigator({
     Page3: {
         screen: Page3,
         navigationOptions: (props) => {
+            //
+            //{"navigation":
+            //    {"state":
+            //        {"params":
+            //            {"title":"Devio"},
+            //            "routeName":"Page3",
+            //            "key":"id-1530501050905-1"},
+            //        "actions":{}
+            //    },
+            //    "screenProps":{},
+            //    "navigationOptions":{}
+            //}
             const {navigation} = props;
             const {state, setParams} = navigation;
             const {params} = state;
             return {
                 title: params.title ? params.title : 'THIS IS Page3',
+                headerRight: (
+                    <Button
+                        title={params.mode === 'edit'? '保存': '编辑'}
+                        onPress={()=>{
+                            setParams({mode: params.mode==='edit' ? '' : 'edit'})
+
+                        }}
+                        />
+                )
+
             }
         }
     }
